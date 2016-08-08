85d82b7a1b7052075d46ec6b7ddd4fea662b9222
DevArc3 <DevArc3@DESKTOP-HSC>
2016-08-08 19:59:42 +0900

    전체화면, 해상도 적용

---
+++ Assets/ResolutionDetection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResolutionDetection : MonoBehaviour
+{
+    public GameObject Resolutionlist;
+    UIPopupList PopupList;
+    UIToggle toggle;
+
+    void Start()
+    {
+        PopupList = GetComponent<UIPopupList>();
+        toggle = transform.parent.GetComponentInChildren<UIToggle>();
+        Resolution[] resolutions = Screen.resolutions;
+        foreach (Resolution res in resolutions)
+        {
+            PopupList.items.Add(res.width + "x" + res.height);
+        }
+    }
+
+    public void OnSelectionChange(string selectedItem)
+    {
+        string[] resolution = selectedItem.Split('x');
+
+        Screen.SetResolution(int.Parse(resolution[0]), int.Parse(resolution[1]), toggle.value);
+    }
+
+    public void ToggleFullScreen(bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
+}
